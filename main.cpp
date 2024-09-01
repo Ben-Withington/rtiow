11e468a7d2d2a7cc96cce4b824fad8eca566eb7c
Ben Withington <benwithington@outlook.com>
2024-09-01 20:21:11 +0100

    done for now

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #include "vec3.h"
 #include "colour.h"
@@ -56,9 +57,8 @@ int main() {
 }
 
 vec3 ray_colour(const ray& r) {
-    if(hit_sphere(vec3{0, 0, -1}, 0.5, r)) {
-        return vec3{ 1, 0, 0 };
-    }
+    if (hit_sphere(vec3(0,0,-1), 0.25, r))
+        return vec3(1, 0, 0);
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -71,5 +71,6 @@ bool hit_sphere(const vec3& centre, double radius, const ray& r) {
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return discriminant >= 0;
+
+    return (discriminant >= 0);
 }
