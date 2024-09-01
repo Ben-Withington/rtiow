1e920ed7fd2ff0c28303a92055f0eaf2e3e8910d
Ben Withington <benwithington@outlook.com>
2024-09-01 19:15:53 +0100

    moved inline function forward declarations

--- vec3.h
+++ vec3.h
@@ -29,15 +29,44 @@ public:
     friend std::ostream& operator<<(std::ostream& out, const vec3& vec);
 };
 
-inline vec3 operator+(const vec3& u, const vec3& v);
-inline vec3 operator-(const vec3& u, const vec3& v);
-inline vec3 operator*(const vec3& u, const vec3& v);
-inline vec3 operator*(double scalar, const vec3& v);
-inline vec3 operator*(const vec3& v, double scalar);
-inline vec3 operator/(const vec3& v, double scalar);
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return { u[0] + v[0], u[1] + v[1], u[2] + v[2] };
+}
 
-inline double dot(const vec3& u, const vec3& v);
-inline vec3 cross(const vec3& u, const vec3& v);
-inline vec3 unit_vector(const vec3& v);
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return { u[0] - v[0], u[1] - v[1], u[2] - v[2] };
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return { u[0] * v[0], u[1] * v[1], u[2] * v[2] };
+}
+
+inline vec3 operator*(double scalar, const vec3& v) {
+    return { scalar * v[0], scalar * v[1], scalar * v[2] };
+}
+
+inline vec3 operator*(const vec3& v, double scalar) {
+    return scalar * v;
+}
+
+inline vec3 operator/(const vec3& v, double scalar) {
+    return (1 / scalar) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return { 
+        u[1] * v[2] - u[2] * v[1],
+        u[2] * v[0] - u[0] * v[2],
+        u[0] * v[1] - u[1] * v[0]
+    };
+}
+
+inline vec3 unit_vector(const vec3& vec) {
+    return vec / vec.length();
+}
 
 #endif // VEC3_H
